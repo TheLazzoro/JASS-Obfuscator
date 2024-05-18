861aae55af7bf45f5a8f4437faa3db00f8bfb7fa
TheLazzoro <lasse.d@live.dk>
2024-05-18 14:20:01 +0200

    Add project files.

---
+++ JASS-Optimizer/JassLexer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JassOptimizer
+{
+    internal static class JassLexer
+    {
+        public static List<Tuple<JassSymbol, string>> Run(string script)
+        {
+            var list = new List<Tuple<JassSymbol, string>>();
+
+            int scriptLength = script.Length;
+            int offset = 0;
+            int i = 0;
+            while (i < scriptLength)
+            {
+                char c = script[i];
+
+                switch (c)
+                {
+                    case '+':
+                    case '-':
+                    case '/':
+                    case '*':
+                        list.Add(new Tuple<JassSymbol, string>(JassSymbol.MathOperator, c.ToString()));
+                        break;
+                    default:
+                        break;
+                }
+
+                i++;
+            }
+
+            return list;
+        }
+    }
+}
---
+++ JASS-Optimizer/JassSymbol.cs
@@ -0,0 +1,14 @@
+﻿namespace JassOptimizer
+{
+    internal enum JassSymbol
+    {
+        None,
+        MathOperator,
+        Keyword_Function,
+        Keyword_EndFunction,
+        Function_Name,
+        Function_Identifier,
+        Takes,
+
+    }
+}
